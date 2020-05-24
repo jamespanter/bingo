f66ab41a11ad3d6013244943fcf1a85f7706af53
jamespanter <james.panter@blueyonder.co.uk>
2020-05-24 23:44:06 +0100

    Basic game logic

---
+++ main.js
@@ -0,0 +1,37 @@
+const output = document.getElementById("output");
+const rollButton = document.getElementById("rollButton");
+const newGameButton = document.getElementById("newGame");
+let bingoNumbers = [];
+let randomIndex = 0;
+
+const newGame = () => {
+  rollButton.style.visibility = "visible";
+  rollButton.style.backgroundColor = "whitesmoke";
+  newGameButton.style.backgroundColor = "whitesmoke";
+  generateNewBingoArray();
+  rollNewNumber();
+};
+
+const generateNewBingoArray = () => {
+  bingoNumbers = [];
+  for (let i = 1; i <= 10; i++) {
+    bingoNumbers.push(i);
+  }
+};
+
+const setRandomIndex = () =>
+  (randomIndex = Math.floor(Math.random() * bingoNumbers.length));
+
+const rollNewNumber = () => {
+  setRandomIndex();
+  output.innerHTML = bingoNumbers[randomIndex];
+  bingoNumbers.splice(randomIndex, 1);
+  checkLose() ? styleLose() : null;
+};
+
+const checkLose = () => bingoNumbers.length == 0;
+
+const styleLose = () => {
+  newGameButton.style.backgroundColor = "lightgreen";
+  rollButton.style.visibility = "hidden";
+};
